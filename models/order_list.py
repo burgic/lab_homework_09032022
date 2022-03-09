691937fd07bb7627661063fb55e06857329f7f28
burgic <ceburgin@gmail.com>
2022-03-09 13:43:10 +0000

    fourth - index set up

--- models/order_list.py
+++ models/order_list.py
@@ -2,3 +2,7 @@ from models.order import *
 
 order1 = Order("Joy", "2/02/22", 5, "Casillero Del Diablo - Merlot")
 order2 = Order("Betty", "13/02/22", 2, "Bollinger")
+order3 = Order("Gillyan", "15/02/22", 1, "Baileys Apple Pie Irish Cream")
+order4 = Order("Dave", "21/02/22", 4, "Beefeater London Dry")
+
+order_list = [order1, order2, order3, order4]
