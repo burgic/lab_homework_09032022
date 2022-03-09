edfb7dc23d0d441ea7a5fe8424fb0ff88dba3651
burgic <ceburgin@gmail.com>
2022-03-09 14:14:23 +0000

    Extension 1 - trying to work out index

--- controllers/controller.py
+++ controllers/controller.py
@@ -5,3 +5,7 @@ from models.order_list import order_list
 @app.route('/orders')
 def index():
     return render_template('index.html', title='WineTopia', order_list=order_list)
+
+@app.route('/order/<index>')
+def get_order(index):
+    return render_template('order.html', title=index, order_list=order_list)
