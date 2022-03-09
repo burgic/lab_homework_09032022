fb8d91341cf621b64c3b2f6b1835e7eae810cc80
burgic <ceburgin@gmail.com>
2022-03-09 13:33:22 +0000

    third - order_list setup

---
+++ controllers/controller.py
@@ -0,0 +1,3 @@
+from flask import render_template
+from app import app
+from models.order_list import order
---
+++ models/order_list.py
@@ -0,0 +1,4 @@
+from models.order import *
+
+order1 = Order("Joy", "2/02/22", 5, "Casillero Del Diablo - Merlot")
+order2 = Order("Betty", "13/02/22", 2, "Bollinger")
